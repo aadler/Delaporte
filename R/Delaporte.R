37020c46a2aa4f1b24b9ea1ea3ce8769e7d35385
Avraham Adler <Avraham.Adler@gmail.com>
2013-04-08 15:08:23 -0400

    Updated documentation

--- R/Delaporte.R
+++ R/Delaporte.R
@@ -1,19 +1,24 @@
-ddelap <- function (x, alpha, beta, lambda, log=FALSE) {
-	DDLAP <- ddelap_C(x, alpha, beta, lambda, log)
-	return(DDLAP)
-}
-
-pdelap <- function (q, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE) {
-	PDLAP <- pdelap_C(q, alpha, beta, lambda, lower.tail, log.p)
-	return(PDLAP)
-}
-
-qdelap <- function (p, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE) {
-  QDLAP <- qdelap_C(p, alpha, beta, lambda, lower.tail, log.p)
-  return(QDLAP)
-}
-
-rdelap <- function (n, alpha, beta, lambda) {
-  RDLAP <- rdelap_C(n, alpha, beta, lambda)
-  return(RDLAP)
-}
+ddelap <-
+  function (x, alpha, beta, lambda, log = FALSE) 
+  {
+    DDLAP <- ddelap_C(x, alpha, beta, lambda, log)
+    return(DDLAP)
+  }
+pdelap <-
+  function (q, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE) 
+  {
+    PDLAP <- pdelap_C(q, alpha, beta, lambda, lower.tail, log.p)
+    return(PDLAP)
+  }
+qdelap <-
+  function (p, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE) 
+  {
+    QDLAP <- qdelap_C(p, alpha, beta, lambda, lower.tail, log.p)
+    return(QDLAP)
+  }
+rdelap <-
+  function (n, alpha, beta, lambda) 
+  {
+    RDLAP <- rdelap_C(n, alpha, beta, lambda)
+    return(RDLAP)
+  }

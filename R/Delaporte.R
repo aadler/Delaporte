8f85181bc8ee05ef585c8a5cb2732a9707e72651
Avraham Adler <Avraham.Adler@gmail.com>
2016-06-16 21:15:22 -0400

    Hotfix reversion due to breaking 0 behavior

--- R/Delaporte.R
+++ R/Delaporte.R
@@ -1,7 +1,5 @@
 ddelap <-
   function (x, alpha, beta, lambda, log = FALSE) {
-    if(any(alpha <= 0) || any(beta <= 0) || any(lambda <= 0))
-      stop('All parameters must be strictly greater than 0.')
     DDLAP <- double(length(x))
     DDLAP <- ddelap_C(x, alpha, beta, lambda, log)
     return(DDLAP)
@@ -9,8 +7,6 @@ ddelap <-
 
 pdelap <-
   function (q, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE) {
-    if(any(alpha <= 0) || any(beta <= 0) || any(lambda <= 0))
-      stop('All parameters must be strictly greater than 0.')
     PDLAP <- double(length(q))
     PDLAP <- pdelap_C(q, alpha, beta, lambda, lower.tail, log.p)
     return(PDLAP)
@@ -18,8 +14,6 @@ pdelap <-
 
 qdelap <-
   function (p, alpha, beta, lambda, lower.tail = TRUE, log.p = FALSE, exact = TRUE) {
-    if(any(alpha <= 0) || any(beta <= 0) || any(lambda <= 0))
-      stop('All parameters must be strictly greater than 0.')
     QDLAP <- double(length(p))
     if (exact) {
       QDLAP <- qdelap_C(p, alpha, beta, lambda, lower.tail, log.p)
@@ -31,9 +25,6 @@ qdelap <-
       p0 <- p[p == 0]
       pInf <- p[p >= 1]
       n <- min(10 ^ (ceiling(log(alpha * beta + lambda, 10)) + 5), 1e7)
-      # For next release; allowing time for deprecation.
-      # ShiftedGammas <- rgamma(n, shape = alpha, scale = beta)
-      # DP <- rpois(n, lambda = (ShiftedGammas + lambda))
       NB <- rnbinom(n, mu = alpha * beta, size = alpha)
       P <- rpois(n, lambda = lambda)
       DP <- NB + P
@@ -41,22 +32,17 @@ qdelap <-
       QNan <- rep.int(NaN, times = length(pNan))
       Q0 <- rep.int(0, times = length(p0))
       QInf <- rep.int(Inf, times = length(pInf))
-      QDLAP <- as.vector(c(QNan, Q0, QValid, QInf), mode = 'numeric')
+      QDLAP <- as.vector(c(QNan, Q0, QValid, QInf), mode = 'integer')
     }  
     return(QDLAP)
   }
 
 rdelap <-
   function (n, alpha, beta, lambda, exact = TRUE) {
-    if(any(alpha <= 0) || any(beta <= 0) || any(lambda <= 0))
-      stop('All parameters must be strictly greater than 0.')
     RDLAP <- double(length(n))
     if (exact) {
       RDLAP <- rdelap_C(n, alpha, beta, lambda)
     } else {
-      # For next release; allowing time for deprecation.
-      # ShiftedGammas <- rgamma(n, shape = alpha, scale = beta)
-      # RDLAP <- rpois(n, lambda = (ShiftedGammas + lambda))
       NB <- rnbinom(max(1e7, n), mu = alpha * beta, size = alpha)
       P <- rpois(max(1e7, n), lambda = lambda)
       DP <- NB + P
@@ -72,7 +58,6 @@ rdelap <-
 MoMdelap <- function (x) {
     MoMDLAP <- double(3)
     MoMDLAP <- MoMdelap_C(x)
-    if (any(MoMDLAP < 0))
-      stop ("Method of moments not appropriate for this data; results include negative parameters.")
+    if (any(MoMDLAP < 0)) stop ("Method of moments not appropriate for this data; results include negative parameters.")
     return(MoMDLAP)
   }

8b05bbde7b21cf705f75f2a794da0c6a4755077a
Avraham Adler <Avraham.Adler@gmail.com>
2016-11-11 10:51:39 -0500

    Add headers, make log a bit more efficient.

--- src/ddelap.f95
+++ src/ddelap.f95
@@ -1,58 +1,88 @@
-module delaporte_f
-use, intrinsic :: iso_c_binding
-use utils
-use lgam
-implicit none
+!----------------------------------------------------------------------------------------
+!
+! MODULE: Delaporte
+!
+! AUTHOR: Avraham Adler <Avraham.Adler@gmail.com>
+!
+! DESCRIPTION: Probability mass, cumulative distribution, and quantile
+!              functions for the Delaporte distribution. Random number
+!              generation and method of moments functions as well.
+!
+! HISTORY:
+!          Version 0.1: 2016-11-11
+!                       Initial porting from C++ code in Delaporte package
+!                       for R.
+!----------------------------------------------------------------------------------------
+module delaporte
+    use, intrinsic :: iso_c_binding
+    use utils
+    use lgam
+
+    implicit none
+    private
+    public :: ddelap_f                      !Vectorized functions called from C
 
 contains
 
-function ddelap_f_s (x, alpha, beta, lambda, lg) result (ddp)
+!----------------------------------------------------------------------------------------
+! FUNCTION: ddelap_f_s
+!
+! DESCRIPTION: Calculate the Delaporte probability mass function for a single observation
+!              and return the value or its log. Calculated through explicit summation.
+!              Will coerce real observations to integer by calling ceiling.
+!----------------------------------------------------------------------------------------
 
-external set_nan
-real(kind = c_double)                      :: ddp
-real(kind = c_double), intent(in)          :: x, alpha, beta, lambda
-logical(kind = c_bool), intent(in)         :: lg
-integer(kind = c_int)                      :: i, k
+    function ddelap_f_s (x, alpha, beta, lambda) result (pmf)
 
-  if (alpha < EPS .or. beta < EPS .or. lambda < EPS .or. x < 0) then
-    call set_nan(ddp)
-  else
-    k = ceiling(x)
-    ddp = 0_c_double
-    do i = 0, k, 1
-      ddp = ddp + exp(gamln(alpha + i) + i * log(beta) &
-                + (k - i) * log(lambda) - lambda &
-                - gamln(alpha) - gamln(i + ONE) &
-                - (alpha + i) * log1p(beta) &
-                - gamln(k - i + ONE))
-    end do
-    if (lg) then
-      ddp = log(ddp)
+    external set_nan                                              ! C-based Nan
+    real(kind = c_double)               :: pmf                    ! Result
+    real(kind = c_double), intent(in)   :: x, alpha, beta, lambda ! Observation & Parms
+!    logical(kind = c_bool), intent(in)  :: lg                     ! Log flag
+    integer                             :: i, k                   ! Integers
+
+    if (alpha < EPS .or. beta < EPS .or. lambda < EPS .or. x < 0) then
+        call set_nan(pmf)
+    else
+        k = ceiling(x)
+        pmf = 0_c_double
+        do i = 0, k, 1
+            pmf = pmf + exp(gamln(alpha + i) + i * log(beta) &
+                      + (k - i) * log(lambda) - lambda &
+                      - gamln(alpha) - gamln(i + ONE) &
+                      - (alpha + i) * log1p(beta) &
+                      - gamln(k - i + ONE))
+        end do
+!        if (lg) then
+!          pmf = log(pmf)
+!        end if
     end if
-  end if
 
 end function ddelap_f_s
 
-subroutine ddelap_f (x, nx, a, na, b, nb, l, nl, lg, ddpv) &
-                   bind(C, name="ddelap_f")
+    !------------------------------------------------------------------------------------
+    ! ROUTINE: ddelap_f
+    !
+    ! DESCRIPTION: Vector-based PMF allowing parameter vector recycling and called from C
+    !------------------------------------------------------------------------------------
 
-integer(kind = c_int), intent(in), value          :: nx, na, nb, nl
-real(kind = c_double), intent(in), dimension(nx)  :: x
-real(kind = c_double), intent(out), dimension(nx) :: ddpv
-real(kind = c_double), intent(in), dimension(na)  :: a
-real(kind = c_double), intent(in), dimension(nb)  :: b
-real(kind = c_double), intent(in), dimension(nl)  :: l
-logical(kind = c_bool), intent(in)                :: lg
-real(kind = c_double), dimension(nx)              :: ax, bx, lx
-integer                                           :: i
+    subroutine ddelap_f (x, nx, a, na, b, nb, l, nl, lg, pmfv) &
+                       bind(C, name="ddelap_f")
 
-  call extend_v(a, nx, na, ax)
-  call extend_v(b, nx, nb, bx)
-  call extend_v(l, nx, nl, lx)
-  do i = 1, nx
-    ddpv(i) = ddelap_f_s(x(i), ax(i), bx(i), lx(i), lg)
-  end do
+    integer(kind = c_int), intent(in), value         :: nx, na, nb, nl     ! Sizes
+    real(kind = c_double), intent(in), dimension(nx) :: x                  ! Observations
+    real(kind = c_double), intent(out), dimension(nx):: pmfv               ! Result
+    real(kind = c_double), intent(in)                :: a(na), b(nb), l(nl)! Parameters
+    logical(kind = c_bool), intent(in)               :: lg                 ! Log flag
+    integer                                          :: i                  ! Integer
 
-end subroutine ddelap_f
+        do i = 1, nx
+            pmfv(i) = ddelap_f_s(x(i), a(mod(i - 1, na) + 1), b(mod(i - 1, nb) + 1), &
+                                 l(mod(i - 1, nl) + 1))
+        end do
+        if (lg) then
+            pmfv = log(pmfv)
+        end if
 
-end module delaporte_f
+    end subroutine ddelap_f
+
+end module delaporte

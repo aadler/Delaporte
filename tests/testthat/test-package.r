0e7898b83c004bd852a47bb1db434aff70421307
Avraham Adler <avraham.adler@gmail.com>
2020-06-01 02:41:40 -0400

    During R CMD check, it is a different directory so this will fail. Need to research how to do it properly.

--- tests/testthat/test-package.r
+++ tests/testthat/test-package.r
@@ -3,7 +3,12 @@ test_that("Version", {
   expect_match(toBibtex(citation('Delaporte')),
                as.character(packageVersion('Delaporte')),
                fixed = TRUE, all = FALSE)
-  expect_match(scan("../../README.md", what = 'character'),
-               as.character(packageVersion('Delaporte')),
-               fixed = TRUE, all = FALSE)
+  # expect_match(scan("../../README.md", what = 'character', skip = 23L,
+  #                   nlines = 1L, quiet = TRUE),
+  #              as.character(packageVersion('Delaporte')),
+  #              fixed = TRUE, all = FALSE)
+  # expect_match(scan("../../README.md", what = 'character', skip = 33L,
+  #                   nlines = 1L, quiet = TRUE),
+  #              as.character(packageVersion('Delaporte')),
+  #              fixed = TRUE, all = FALSE)
 })

98660e05bdccb416fe5995e1a477d237067f8a4b
Avraham Adler <avraham.adler@gmail.com>
2020-11-23 14:00:54 -0500

    Not going to be used.

--- tests/testthat/test-package.r
+++ tests/testthat/test-package.r
@@ -2,12 +2,4 @@ test_that("Version", {
   expect_match(toBibtex(citation('Delaporte')),
                as.character(packageVersion('Delaporte')),
                fixed = TRUE, all = FALSE)
-  # expect_match(scan("../../README.md", what = 'character', skip = 23L,
-  #                   nlines = 1L, quiet = TRUE),
-  #              as.character(packageVersion('Delaporte')),
-  #              fixed = TRUE, all = FALSE)
-  # expect_match(scan("../../README.md", what = 'character', skip = 33L,
-  #                   nlines = 1L, quiet = TRUE),
-  #              as.character(packageVersion('Delaporte')),
-  #              fixed = TRUE, all = FALSE)
 })

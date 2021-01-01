b1fdf7b7af3aa5dd3723cb004686026e2c98bf60
Avraham Adler <avraham.adler@gmail.com>
2021-01-01 11:57:35 -0500

    Will only use when building locally, otheriwse will skip b/c directory structure is different.

--- tests/testthat/test-package.r
+++ tests/testthat/test-package.r
@@ -1,5 +1,17 @@
-test_that("Version", {
+test_that("Citation", {
   expect_match(toBibtex(citation('Delaporte')),
+               as.character(packageVersion('Delaporte')),
+               fixed = TRUE, all = FALSE)
+})
+
+skip("Different structure")
+test_that("README", {
+  expect_match(scan("../../README.md", what = 'character', skip = 23L,
+                    nlines = 1L, quiet = TRUE),
+               as.character(packageVersion('Delaporte')),
+               fixed = TRUE, all = FALSE)
+  expect_match(scan("../../README.md", what = 'character', skip = 33L,
+                    nlines = 1L, quiet = TRUE),
                as.character(packageVersion('Delaporte')),
                fixed = TRUE, all = FALSE)
 })

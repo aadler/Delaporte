d407bf8f357364b225406b16ade5e3cc547b2050
Avraham Adler <avraham.adler@gmail.com>
2023-08-04 17:35:42 -0400

    Update with more version tests

--- inst/tinytest/test-citation.r
+++
@@ -1,3 +0,0 @@
-# Test Citation
-expect_true(any(grepl(packageVersion("Delaporte"),
-                      toBibtex(citation("Delaporte")), fixed = TRUE)))
---
+++ inst/tinytest/test-version.r
@@ -0,0 +1,11 @@
+pV <- packageVersion("Delaporte")
+
+# Test CITATION has most recent package version
+expect_true(any(grepl(pV, toBibtex(citation("Delaporte")), fixed = TRUE)))
+
+# Test NEWS has most recent package version
+expect_true(any(grepl(pV, news(package = "Delaporte"), fixed = TRUE)))
+
+# Test that NEWS has an entry with DESCRIPTION's Date
+expect_true(any(grepl(packageDate("Delaporte"), news(package = "Delaporte"),
+                      fixed = TRUE)))

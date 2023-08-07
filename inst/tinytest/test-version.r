01a5cf3e5e494137cbb1faa85715dd6b7665a431
Avraham Adler <avraham.adler@gmail.com>
2023-08-07 13:52:51 -0400

    slowed down Debian way too much

--- inst/tinytest/test-version.r
+++ inst/tinytest/test-version.r
@@ -6,9 +6,15 @@ pV <- packageVersion("Delaporte")
 # Test CITATION has most recent package version
 expect_true(any(grepl(pV, toBibtex(citation("Delaporte")), fixed = TRUE)))
 
-# Test NEWS has most recent package version
-expect_true(any(grepl(pV, news(package = "Delaporte"), fixed = TRUE)))
+# For some reason this passes on Windows and Mac but slows down Debian. Since
+# this is mainly for my own personal development purposes, I will wrap it in a
+# test for HOME.
 
-# Test that NEWS has an entry with DESCRIPTION's Date
-expect_true(any(grepl(packageDate("Delaporte"), news(package = "Delaporte"),
-                      fixed = TRUE)))
+if (Sys.info()["nodename"] == "HOME") {
+  # Test NEWS has most recent package version
+  expect_true(any(grepl(pV, news(package = "Delaporte"), fixed = TRUE)))
+  
+  # Test that NEWS has an entry with DESCRIPTION's Date
+  expect_true(any(grepl(packageDate("Delaporte"), news(package = "Delaporte"),
+                        fixed = TRUE)))
+}

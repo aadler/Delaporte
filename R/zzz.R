fd243c992b209dda5ddfcd802388b8e5cb8ed163
Avraham Adler <avraham.adler@gmail.com>
2023-08-08 16:36:26 -0400

    Try and capture reasonable maximum ncpu using recommended package parallel. Also clean up options when unloading.

---
+++ R/zzz.R
@@ -0,0 +1,17 @@
+# nocov start
+.onLoad <- function(libname, pkgname) {
+  # Get reasonable selction for "max" cpus
+  options("DLPCPU" = as.integer(getOption("DLPCPU",
+                                          default = parallel::detectCores())))
+}
+
+.onDetach <- function(libpath) {
+  # Restore reasonable option and remove holding variable
+  setThreads(getOption("DLPCPU"))
+  options("DLPCPU" = NULL)
+}
+
+.onUnload <- function(libpath) {
+  library.dynam.unload("Delaporte", libpath)
+}
+# nocov end

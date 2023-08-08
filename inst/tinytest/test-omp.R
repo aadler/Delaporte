442f468ca71a2aea6d2cd192713a4e61d6cd3465
Avraham Adler <avraham.adler@gmail.com>
2023-08-08 17:04:33 -0400

    Need to consider not every test machine is multicore.

--- inst/tinytest/test-omp.R
+++ inst/tinytest/test-omp.R
@@ -1,12 +1,12 @@
 # Copyright (c) 2023, Avraham Adler All rights reserved
 # SPDX-License-Identifier: BSD-2-Clause
 
-expect_silent(setDelapThreads(4L))
-expect_identical(getDelapThreads(), 4L)
+expect_silent(setDelapThreads(1L))
+expect_identical(getDelapThreads(), 1L)
 expect_error(setDelapThreads(0L), "Number of threads must be >= 0.")
 expect_error(setDelapThreads(-2L), "Number of threads must be >= 0.")
-expect_silent(setDelapThreads(3.9))
-expect_identical(getDelapThreads(), 3L)
+expect_silent(setDelapThreads(1.9))
+expect_identical(getDelapThreads(), 1L)
 capMsg <- paste0("Capping at system maximum of ", getOption("DLPCPU"), ".")
 expect_message(setDelapThreads(1024^2), capMsg)
 expect_identical(getDelapThreads(), getOption("DLPCPU"))

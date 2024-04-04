b9a6d58126e187c63911fa328562431445b1c742
Avraham Adler <avraham.adler@gmail.com>
2024-04-04 14:13:48 -0400

    False positive in linter

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 # nocov start
 .onLoad <- function(libname, pkgname) {
   # Get reasonable selction for "max" cpus
-  options("DLPCPU" = as.integer(
+  options("DLPCPU" = as.integer( # nolint
     getOption("DLPCPU", default = parallel::detectCores())
   ))
 }
@@ -9,7 +9,7 @@
 .onDetach <- function(libpath) {
   # Restore reasonable option and remove holding variable
   setDelapThreads(getOption("DLPCPU"))
-  options("DLPCPU" = NULL)
+  options("DLPCPU" = NULL) # nolint
 }
 
 .onUnload <- function(libpath) {

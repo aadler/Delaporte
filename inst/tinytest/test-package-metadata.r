68f290c876fc1958016ab4667f18b78f042852fb
Avraham Adler <avraham.adler@gmail.com>
2024-05-21 16:10:45 -0400

    Update tests per other packages, but make code more universal. Will copy to other packages in due time.

--- inst/tinytest/test-package-metadata.r
+++ inst/tinytest/test-package-metadata.r
@@ -1,19 +1,38 @@
 # Copyright (c) 2013, Avraham Adler All rights reserved
 # SPDX-License-Identifier: BSD-2-Clause
 
-pV <- packageVersion("Delaporte")
-
-# Test CITATION has most recent package version
-expect_true(any(grepl(pV, toBibtex(citation("Delaporte")), fixed = TRUE)))
-
-# For some reason this passes on Windows and Mac but slows down Debian. Since
-# this is mainly for my own personal development purposes, I will wrap it in a
-# test for HOME.
+# Only test at home. rhub valgrind complains and it should not affect covr.
 
 if (Sys.info()["nodename"] == "HOME") {
+  
+  # Setup
+  myPkgs <- c("Delaporte",
+              "lamW",
+              "Pade",
+              "revss",
+              "minimaxApprox",
+              "MBBEFDLite")
+  thisPkg <- "Delaporte"
+  otrPkgs <- setdiff(myPkgs, thisPkg)
+
+  # Universal code
+  pV <- packageVersion(thisPkg)
+  pD <- packageDate(thisPkg)
+  cit <- toBibtex(citation(thisPkg))
+  nws <- news(package = thisPkg)
+
+  # Test CITATION has most recent package version
+  expect_true(any(grepl(pV, cit), fixed = TRUE))
+  
   # Test NEWS has most recent package version
-  expect_true(any(grepl(pV, news(package = "Delaporte"), fixed = TRUE)))
+  expect_true(any(grepl(pV, nws, fixed = TRUE)))
+  
   # Test that NEWS has an entry with DESCRIPTION's Date
-  expect_true(any(grepl(packageDate("Delaporte"), news(package = "Delaporte"),
-                        fixed = TRUE)))
+  expect_true(any(grepl(pD, nws, fixed = TRUE)))
+  
+  # Test that CITATION doesn't contain the name of any other of my packages
+  expect_false(any(sapply(otrPkgs, grepl, x = cit, fixed = TRUE)))
+  
+  # Test that NEWS doesn't contain the name of any other of my packages
+  expect_false(any(sapply(otrPkgs, grepl, x = nws, fixed = TRUE)))
 }

35d04eaa0651005ea1db7ea82da72bbe10630d88
Avraham Adler <Avraham.Adler@gmail.com>
2025-07-16 22:41:51 -0400

    Use environment variable instead of options per Professor Ripley and SVN 88406.

--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,19 @@
 # nocov start
 .onLoad <- function(libname, pkgname) {
-  # Get reasonable selection for "max" cpus
-  options("DLPCPU" = as.integer( # nolint
-    getOption("DLPCPU", default = parallel::detectCores())
-  ))
+  # Get reasonable selection for "max" cpus and store in enviornment (per 88406)
+  DelaporteEnv <<- new.env(parent = emptyenv())
+  if (!exists("DLPCPU", envir = DelaporteEnv)) {
+    assign("DLPCPU", parallel::detectCores(), envir = DelaporteEnv)
+  }
 }
 
 .onDetach <- function(libpath) {
-  # Restore reasonable option and remove holding variable
-  setDelapThreads(getOption("DLPCPU"))
-  options("DLPCPU" = NULL) # nolint
+  # Restore reasonable option and remove holding variable and environment.
+  setDelapThreads(get("DLPCPU", envir = DelaporteEnv))
 }
 
 .onUnload <- function(libpath) {
+  rm(DelaporteEnv, pos = 1)
   library.dynam.unload("Delaporte", libpath)
 }
 # nocov end
--- inst/tinytest/test-omp.R
+++ inst/tinytest/test-omp.R
@@ -7,5 +7,6 @@ expect_error(setDelapThreads(0L), "Number of threads must be > 0.")
 expect_error(setDelapThreads(-2L), "Number of threads must be > 0.")
 expect_silent(setDelapThreads(1.9))
 expect_identical(getDelapThreads(), 1L)
-capMsg <- paste0("Capping at system maximum of ", getOption("DLPCPU"), ".")
+capMsg <- paste0("Capping at system maximum of ",
+                 get("DLPCPU", envir = DelaporteEnv), ".")
 expect_message(setDelapThreads(1024^2), capMsg)

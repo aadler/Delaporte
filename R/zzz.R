432f400a9c74dc32b3949158d594edb79d8c74bf
Avraham Adler <Avraham.Adler@gmail.com>
2025-12-24 02:52:11 -0500

    Add copyright notice and clean comments.

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,9 @@
+# Copyright (c) 2023, Avraham Adler All rights reserved
+# SPDX-License-Identifier: BSD-2-Clause
+
 # nocov start
 .onLoad <- function(libname, pkgname) {
-  # Get reasonable selection for "max" cpus and store in enviornment (per 88406)
+  # Get reasonable selection for "max" cpus and store in environment (per 88406)
   DelaporteEnv <<- new.env(parent = emptyenv()) # nolint object_name_linter
   if (!exists("DLPCPU", envir = DelaporteEnv)) {
     assign("DLPCPU", parallel::detectCores(), envir = DelaporteEnv)
@@ -8,11 +11,12 @@
 }
 
 .onDetach <- function(libpath) {
-  # Restore reasonable option and remove holding variable and environment.
+  # Restore reasonable option
   setDelapThreads(get("DLPCPU", envir = DelaporteEnv))
 }
 
 .onUnload <- function(libpath) {
+  # Remove holding variable and environment
   rm(DelaporteEnv, pos = 1)
   library.dynam.unload("Delaporte", libpath)
 }
